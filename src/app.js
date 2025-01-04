6e7de775452740d6666da21da048388e4551cbc6
ayushi376 <ayushiagg2000@gmail.com>
2025-01-04 20:16:59 +0530

    exploring get post delete patch

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express=require("express");
 const app=express();
-app.use("/",(req,res)=>{
-    res.send("hi node.js");
+// This will only handle get call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Ayushi",lastname:"Agarwal"});
 });
-app.use("/hello",(req,res)=>{
-    res.send("hello node.js");
+app.post("/user",(req,res)=>{
+    res.send("Data successfully saved in database ");
 });
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully");
+});
+app.patch("/user",(req,res)=>{
+    res.send("succesfully patch");
+});
+// This will match all the HTTP method API call /test
 app.use("/text",(req,res)=>{
     res.send("text node.js");
 });
+
 app.listen(3000,()=>{
     console.log("listen sucessfully on port number 3000");
 
